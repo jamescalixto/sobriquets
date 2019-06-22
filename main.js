00021cb61a6adb8198dadd63d11dd2936f363fb4
jamescalixto <jc2386@cornell.edu>
2019-06-21 20:47:14 -0400

    added cards

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
-function test() {
-    console.log("test");
+function testBuild() {
+  NUM_CARDS = 25;
+  let card_template = document.getElementById("card-template");
+  for (let i = 0; i < NUM_CARDS; i++) {
+    var clone = card_template.cloneNode(true); // Make copy.
+    clone.style.display = "inline-flex"; // Make copy visible.
+    document.getElementById("board-container").appendChild(clone);
+  }
 }
