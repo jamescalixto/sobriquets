9d13b6f345384735e1d6264d6b6f0156764e88b1
jamescalixto <jc2386@cornell.edu>
2019-06-22 03:10:13 -0400

    added non-spymaster toggling

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ const card_states = {
   ASSASSIN: "assassin"
 };
 
+
 // Given a number n, get k random numbers in the range
 // [0,n) without repetition. Precondition that n >= k.
 // Utilizes pseudorandom generator, with seed s.
@@ -22,17 +23,28 @@ function getRandomNumbersWithoutRepetition(s, n, k) {
   return random_numbers;
 }
 
+
 // Check who goes first, based on the hash password.
 function doesRedGoFirst(hash_password) {
   let rng = new alea(hash_password + hash_password);
-  return (rng() < 0.5);
+  return rng() < 0.5;
 }
+
+
+// Delete existing cards.
+function deleteExistingCards() {
+  let board_container = document.getElementById("board-container");
+  while (board_container.firstChild) {
+    board_container.removeChild(board_container.firstChild);
+  }
+}
+
 
 // Function to test build.
 function buildNewBoard(hash_name, hash_password, word_list, isSpymaster) {
   // Remove the cards that already exist.
   deleteExistingCards();
-  
+
   // Default to 5x5 grid.
   const NUM_CARDS = 25;
 
@@ -70,15 +82,20 @@ function buildNewBoard(hash_name, hash_password, word_list, isSpymaster) {
     clone.children.namedItem("card-label").textContent =
       word_list[word_array[i]];
 
-    // Set card state.
-    if (identity_array[i] < NUM_ASSASSIN_CARDS) {
-      clone.setAttribute("state", card_states.ASSASSIN);
-    } else if (identity_array[i] < NUM_ASSASSIN_CARDS + NUM_RED_CARDS) {
-      clone.setAttribute("state", card_states.RED);
-    } else if (identity_array[i] < NUM_ASSASSIN_CARDS + NUM_RED_CARDS + NUM_BLUE_CARDS) {
-      clone.setAttribute("state", card_states.BLUE);
-    } else {
-      clone.setAttribute("state", card_states.BYSTANDER);
+    // Set card identity. Only called if the player is the spymaster.
+    function setCardIdentity() {
+      if (identity_array[i] < NUM_ASSASSIN_CARDS) {
+        clone.setAttribute("state", card_states.ASSASSIN);
+      } else if (identity_array[i] < NUM_ASSASSIN_CARDS + NUM_RED_CARDS) {
+        clone.setAttribute("state", card_states.RED);
+      } else if (
+        identity_array[i] <
+        NUM_ASSASSIN_CARDS + NUM_RED_CARDS + NUM_BLUE_CARDS
+      ) {
+        clone.setAttribute("state", card_states.BLUE);
+      } else {
+        clone.setAttribute("state", card_states.BYSTANDER);
+      }
     }
 
     // If we are the spymaster: display card states and
@@ -86,41 +103,43 @@ function buildNewBoard(hash_name, hash_password, word_list, isSpymaster) {
     // Otherwise, set up the click callback to toggle card
     // state.
     if (isSpymaster) {
+      setCardIdentity();
       setCardStyle(clone);
     } else {
+      // Set state to default.
+      clone.setAttribute("state", card_states.DEFAULT);
 
+      // Add toggling for state.
+      clone.addEventListener("click", function() {
+        const current_state = clone.getAttribute("state");
+        clone.classList.remove("styling-" + current_state);
+        clone.setAttribute("state", cycleEnum(card_states, current_state));
+        console.log(current_state, cycleEnum(card_states, current_state))
+        setCardStyle(clone);
+      })
     }
 
     document.getElementById("board-container").appendChild(clone);
   }
 }
 
+
 // Add card style based on state attribute.
 function setCardStyle(clone) {
-  // Set card styling based on state.
-  switch (clone.getAttribute("state")) {
-    case card_states.ASSASSIN:
-      clone.className += " styling-assassin";
-      break;
-    case card_states.RED:
-      clone.className += " styling-red";
-      break;
-    case card_states.BLUE:
-      clone.className += " styling-blue";
-      break;
-    case card_states.BYSTANDER:
-      clone.className += " styling-bystander";
-      break;
-    case card_states.DEFAULT:
-      clone.className += " styling-default";
-      break;
-  }
+  const current_state = clone.getAttribute("state");
+  clone.classList.add("styling-" + current_state);
 }
 
-// Delete existing cards.
-function deleteExistingCards() {
-  let board_container = document.getElementById("board-container");
-  while (board_container.firstChild) {
-    board_container.removeChild(board_container.firstChild);
+
+// Given an enumerated type and one of its values,
+// get the next value as defined. Loops around to
+// the first value if necessary.
+function cycleEnum(enum_var, value) {
+  const enum_properties = [];
+  for (property in enum_var) {
+    enum_properties.push(property);
   }
+  const enum_index =
+    (Object.values(enum_var).indexOf(value) + 1) % enum_properties.length;
+  return enum_var[enum_properties[enum_index]];
 }
